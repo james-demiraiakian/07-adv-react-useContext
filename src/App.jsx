f8cd2c12caddc8fe8cddafb1589cbcdbd9b2d27f
James Demiraiakian <james.armen@gmail.com>
2022-02-07 12:40:11 -0800

    fin but not. removed some unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import Layout from './views/Layout/Layout'
 import Home from './views/Home/Home'
-import fetchUser from './services/user'
 
 function App() {
   // inital value should match the data type of end value

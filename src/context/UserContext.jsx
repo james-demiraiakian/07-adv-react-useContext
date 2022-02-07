0cb677a48bf69c9f63a922ee751cad54a33a400c
James Demiraiakian <james.armen@gmail.com>
2022-02-07 12:02:15 -0800

    Wrote UserProvider function

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,7 +1,11 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext, useMemo, useState } from 'react'
 
 export const UserContext = createContext()
 
-const UserProvider = ({children}) => {
-    
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState('')
+
+  const value = useMemo(() => ({ user, setUser }), [user])
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
